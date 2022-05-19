a4b921aee0d5a5857119e683103becebb36cb441
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-05-19 17:55:08 +0530

    Plant page edited

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Plant() {
   return (
-    <div className='plant'>Plant</div>
+    <div className='plant__container'>
+    <div className='plant'>
+      <img src='' alt=''/>
+      <p></p>
+      <h3></h3>
+    </div>
+    </div>
   )
 }
 
